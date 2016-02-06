2566c29d47fadd31a9b769d5b5d0ed80ba2fb306
iostreamer-X <iostreamer007@gmail.com>
2016-02-06 23:15:03 +0530

    web sockets ftw

--- client.py
+++ client.py
@@ -1,7 +1,32 @@
-from socketIO_client import SocketIO
+import websocket
+import thread
+import time
 
-def on_aaa_response(*args):
-    print 'on_aaa_response', args
+def on_message(ws, message):
+    print message
 
-socketIO = SocketIO('localhost', 6000)
-socketIO.on('futurelabs', on_aaa_response)
+def on_error(ws, error):
+    print error
+
+def on_close(ws):
+    print "### closed ###"
+
+def on_open(ws):
+    def run(*args):
+        for i in range(3):
+            time.sleep(1)
+            ws.send("Hello %d" % i)
+        time.sleep(1)
+        ws.close()
+        print "thread terminating..."
+    thread.start_new_thread(run, ())
+
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://localhost:6001",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
